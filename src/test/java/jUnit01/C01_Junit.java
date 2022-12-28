e67367729713f41be666a546215c7fda950aace7
serkananduse <fatser2701@gmail.com>
2022-12-28 22:41:59 +0100

    quick

--- src/test/java/jUnit01/C01_Junit.java
+++ src/test/java/jUnit01/C01_Junit.java
@@ -1,4 +1,20 @@
 package jUnit01;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class C01_Junit {
+    WebDriver driver;
+    @Before
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+    }
+
 }
